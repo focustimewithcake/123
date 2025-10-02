af5843587e7f07a3268af32fb4a237bb6f35602f
focustimewithcake <thuyvan010120081@gmail.com>
2025-10-02 14:18:31 +0700

    Create momo-ipn.js

---
+++ netlify/functions/momo-ipn.js
@@ -0,0 +1,12 @@
+const crypto = require('crypto');
+const db = require('./db.js');
+
+const MOMO_SECRET_KEY = process.env.MOMO_SECRET_KEY || "PPuXqPk5fvJVEKtM1G0oQf0pVl2XCxx6";
+
+function verifySignature(data) {
+  // ... (code đầy đủ từ trước)
+}
+
+exports.handler = async (event) => {
+  // ... (code handler đầy đủ từ trước)
+};
